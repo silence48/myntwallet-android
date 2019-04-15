0fb6da3d9731c846a866165c9e73cdc8fb07b305
silence48 <35656692+silence48@users.noreply.github.com>
2019-04-15 17:13:04 -0400

    convert to x16s

--- app/src/main/jni/core/BRCrypto.c
+++ app/src/main/jni/core/BRCrypto.c
@@ -222,157 +222,172 @@ void BRSHA256_2(void *md32, const void *data, size_t len)
     BRSHA256(md32, t, sizeof(t));
 }
 
-int GetNibble(const uint8_t *data, int index)
-{
-    index = 63 - index;
-    if (index % 2 == 1)
-        return(data[index / 2] >> 4);
-    return(data[index / 2] & 0x0F);
-}
-
-int GetHashSelection(const uint8_t *PrevBlockHash, int index) {
-    assert(index >= 0);
-    assert(index < 16);
-    
-#define START_OF_LAST_16_NIBBLES_OF_HASH 48
-    int hashSelection = GetNibble(PrevBlockHash, START_OF_LAST_16_NIBBLES_OF_HASH + index);
-    return(hashSelection);
-}
-
 void trim512to256(void* in_512, void* out_256)
 {
     memcpy((void*)out_256, (void*)in_512, 32);
 }
 
-void BRX16R(void *md32, const void *data, size_t len)
+enum Algo {
+	BLAKE = 0,
+	BMW,
+	GROESTL,
+	JH,
+	KECCAK,
+	SKEIN,
+	LUFFA,
+	CUBEHASH,
+	SHAVITE,
+	SIMD,
+	ECHO,
+	HAMSI,
+	FUGUE,
+	SHABAL,
+	WHIRLPOOL,
+	SHA512,
+	HASH_FUNC_COUNT
+};
+
+static void getAlgoString(const uint8_t* prevblock, char *output)
 {
-    assert(md32 != NULL);
-    assert(data != NULL || len == 0);
+    strcpy(output, "0123456789ABCDEF");
+
+	for(int i = 0; i < 16; i++){
+		uint8_t b = (15 - i) >> 1; // 16 ascii hex chars, reversed
+		uint8_t algoDigit = (i & 1) ? prevblock[b] & 0xF : prevblock[b] >> 4;
+
+		int offset = algoDigit;
+		// insert the nth character at the front
+		char oldVal = output[offset];
+		for(int j=offset; j-->0;) {
+			output[j+1] = output[j];
+		}
+		output[0] = oldVal;
+	}
+}
+
+void BRX16S(void *output, const void *input, size_t len)
+{
+	assert(input != NULL);
+    assert(output != NULL || len == 0);
 
     int hashSelection;
 
-    sph_blake512_context     ctx_blake;      //0
-    sph_bmw512_context       ctx_bmw;        //1
-    sph_groestl512_context   ctx_groestl;    //2
-    sph_jh512_context        ctx_jh;         //3
-    sph_keccak512_context    ctx_keccak;     //4
-    sph_skein512_context     ctx_skein;      //5
-    sph_luffa512_context     ctx_luffa;      //6
-    sph_cubehash512_context  ctx_cubehash;   //7
-    sph_shavite512_context   ctx_shavite;    //8
-    sph_simd512_context      ctx_simd;       //9
-    sph_echo512_context      ctx_echo;       //A
-    sph_hamsi512_context     ctx_hamsi;      //B
-    sph_fugue512_context     ctx_fugue;      //C
-    sph_shabal512_context    ctx_shabal;     //D
-    sph_whirlpool_context    ctx_whirlpool;  //E
-    sph_sha512_context       ctx_sha512;     //F
+    uint32_t hash[64/4];
+	char hashOrder[HASH_FUNC_COUNT + 1] = { 0 };
 
-    uint8_t *PrevBlockHash = (uint8_t*) &(data[4]);
+	sph_blake512_context     ctx_blake;
+	sph_bmw512_context       ctx_bmw;
+	sph_groestl512_context   ctx_groestl;
+	sph_skein512_context     ctx_skein;
+	sph_jh512_context        ctx_jh;
+	sph_keccak512_context    ctx_keccak;
+	sph_luffa512_context     ctx_luffa;
+	sph_cubehash512_context  ctx_cubehash;
+	sph_shavite512_context   ctx_shavite;
+	sph_simd512_context      ctx_simd;
+	sph_echo512_context      ctx_echo;
+	sph_hamsi512_context     ctx_hamsi;
+	sph_fugue512_context     ctx_fugue;
+	sph_shabal512_context    ctx_shabal;
+	sph_whirlpool_context    ctx_whirlpool;
+	sph_sha512_context       ctx_sha512;
 
-    uint8_t hash_temp[64];
+	void *in = (void*) input;
+	int size = len;
 
-    //const uint256 scrambleHash = uint256S(hashFront + order); // uint256 with length of hash and shuffled last sixteen
+	getAlgoString(&input[4], hashOrder);
 
-    for (int i=0;i<16;i++)
-    {
-        const void *toHash;
-        size_t lenToHash;
-        if (i == 0) {
-            toHash = data;
-            lenToHash = len;
-        } else {
-            toHash = hash_temp;
-            lenToHash = 64;
-        }
-        //hashSelection = GetHashSelection(scrambleHash, i);
-        hashSelection = GetHashSelection(PrevBlockHash, i);
+	for (int i = 0; i < 16; i++)
+	{
+		const char elem = hashOrder[i];
+		const uint8_t algo = elem >= 'A' ? elem - 'A' + 10 : elem - '0';
 
-        switch(hashSelection) {
-            case 0:
-                sph_blake512_init(&ctx_blake);
-                sph_blake512 (&ctx_blake, toHash, lenToHash);
-                sph_blake512_close(&ctx_blake, hash_temp);
-                break;
-            case 1:
-                sph_bmw512_init(&ctx_bmw);
-                sph_bmw512 (&ctx_bmw, toHash, lenToHash);
-                sph_bmw512_close(&ctx_bmw, hash_temp);
-                break;
-            case 2:
-                sph_groestl512_init(&ctx_groestl);
-                sph_groestl512 (&ctx_groestl, toHash, lenToHash);
-                sph_groestl512_close(&ctx_groestl, hash_temp);
-                break;
-            case 3:
-                sph_jh512_init(&ctx_jh);
-                sph_jh512 (&ctx_jh, toHash, lenToHash);
-                sph_jh512_close(&ctx_jh, hash_temp);
-                break;
-            case 4:
-                sph_keccak512_init(&ctx_keccak);
-                sph_keccak512 (&ctx_keccak, toHash, lenToHash);
-                sph_keccak512_close(&ctx_keccak, hash_temp);
-                break;
-            case 5:
-                sph_skein512_init(&ctx_skein);
-                sph_skein512 (&ctx_skein, toHash, lenToHash);
-                sph_skein512_close(&ctx_skein, hash_temp);
-                break;
-            case 6:
-                sph_luffa512_init(&ctx_luffa);
-                sph_luffa512 (&ctx_luffa, toHash, lenToHash);
-                sph_luffa512_close(&ctx_luffa, hash_temp);
-                break;
-            case 7:
-                sph_cubehash512_init(&ctx_cubehash);
-                sph_cubehash512 (&ctx_cubehash, toHash, lenToHash);
-                sph_cubehash512_close(&ctx_cubehash, hash_temp);
-                break;
-            case 8:
-                sph_shavite512_init(&ctx_shavite);
-                sph_shavite512(&ctx_shavite, toHash, lenToHash);
-                sph_shavite512_close(&ctx_shavite, hash_temp);
-                break;
-            case 9:
-                sph_simd512_init(&ctx_simd);
-                sph_simd512 (&ctx_simd, toHash, lenToHash);
-                sph_simd512_close(&ctx_simd, hash_temp);
-                break;
-            case 10:
-                sph_echo512_init(&ctx_echo);
-                sph_echo512 (&ctx_echo, toHash, lenToHash);
-                sph_echo512_close(&ctx_echo, hash_temp);
-                break;
-            case 11:
-                sph_hamsi512_init(&ctx_hamsi);
-                sph_hamsi512 (&ctx_hamsi, toHash, lenToHash);
-                sph_hamsi512_close(&ctx_hamsi, hash_temp);
-                break;
-            case 12:
-                sph_fugue512_init(&ctx_fugue);
-                sph_fugue512 (&ctx_fugue, toHash, lenToHash);
-                sph_fugue512_close(&ctx_fugue, hash_temp);
-                break;
-            case 13:
-                sph_shabal512_init(&ctx_shabal);
-                sph_shabal512 (&ctx_shabal, toHash, lenToHash);
-                sph_shabal512_close(&ctx_shabal, hash_temp);
-                break;
-            case 14:
-                sph_whirlpool_init(&ctx_whirlpool);
-                sph_whirlpool(&ctx_whirlpool, toHash, lenToHash);
-                sph_whirlpool_close(&ctx_whirlpool, hash_temp);
-                break;
-            case 15:
-                sph_sha512_init(&ctx_sha512);
-                sph_sha512 (&ctx_sha512, toHash, lenToHash);
-                sph_sha512_close(&ctx_sha512, hash_temp);
-                break;
-        }
-    }
-    
-    trim512to256(hash_temp, md32);
+		switch (algo) {
+		case BLAKE:
+			sph_blake512_init(&ctx_blake);
+			sph_blake512(&ctx_blake, in, size);
+			sph_blake512_close(&ctx_blake, hash);
+			break;
+		case BMW:
+			sph_bmw512_init(&ctx_bmw);
+			sph_bmw512(&ctx_bmw, in, size);
+			sph_bmw512_close(&ctx_bmw, hash);
+			break;
+		case GROESTL:
+			sph_groestl512_init(&ctx_groestl);
+			sph_groestl512(&ctx_groestl, in, size);
+			sph_groestl512_close(&ctx_groestl, hash);
+			break;
+		case SKEIN:
+			sph_skein512_init(&ctx_skein);
+			sph_skein512(&ctx_skein, in, size);
+			sph_skein512_close(&ctx_skein, hash);
+			break;
+		case JH:
+			sph_jh512_init(&ctx_jh);
+			sph_jh512(&ctx_jh, in, size);
+			sph_jh512_close(&ctx_jh, hash);
+			break;
+		case KECCAK:
+			sph_keccak512_init(&ctx_keccak);
+			sph_keccak512(&ctx_keccak, in, size);
+			sph_keccak512_close(&ctx_keccak, hash);
+			break;
+		case LUFFA:
+			sph_luffa512_init(&ctx_luffa);
+			sph_luffa512(&ctx_luffa, in, size);
+			sph_luffa512_close(&ctx_luffa, hash);
+			break;
+		case CUBEHASH:
+			sph_cubehash512_init(&ctx_cubehash);
+			sph_cubehash512(&ctx_cubehash, in, size);
+			sph_cubehash512_close(&ctx_cubehash, hash);
+			break;
+		case SHAVITE:
+			sph_shavite512_init(&ctx_shavite);
+			sph_shavite512(&ctx_shavite, in, size);
+			sph_shavite512_close(&ctx_shavite, hash);
+			break;
+		case SIMD:
+			sph_simd512_init(&ctx_simd);
+			sph_simd512(&ctx_simd, in, size);
+			sph_simd512_close(&ctx_simd, hash);
+			break;
+		case ECHO:
+			sph_echo512_init(&ctx_echo);
+			sph_echo512(&ctx_echo, in, size);
+			sph_echo512_close(&ctx_echo, hash);
+			break;
+		case HAMSI:
+			sph_hamsi512_init(&ctx_hamsi);
+			sph_hamsi512(&ctx_hamsi, in, size);
+			sph_hamsi512_close(&ctx_hamsi, hash);
+			break;
+		case FUGUE:
+			sph_fugue512_init(&ctx_fugue);
+			sph_fugue512(&ctx_fugue, in, size);
+			sph_fugue512_close(&ctx_fugue, hash);
+			break;
+		case SHABAL:
+			sph_shabal512_init(&ctx_shabal);
+			sph_shabal512(&ctx_shabal, in, size);
+			sph_shabal512_close(&ctx_shabal, hash);
+			break;
+		case WHIRLPOOL:
+			sph_whirlpool_init(&ctx_whirlpool);
+			sph_whirlpool(&ctx_whirlpool, in, size);
+			sph_whirlpool_close(&ctx_whirlpool, hash);
+			break;
+		case SHA512:
+			sph_sha512_init(&ctx_sha512);
+			sph_sha512(&ctx_sha512,(const void*) in, size);
+			sph_sha512_close(&ctx_sha512,(void*) hash);
+			break;
+		}
+		in = (void*) hash;
+		size = 64;
+	}
+	trim512to256(hash, output);
 }
 
 
